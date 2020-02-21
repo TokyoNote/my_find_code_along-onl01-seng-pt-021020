e4b8ef94f3d18f2c5d54052ec9af3ceec222f49f
Camiyah Goins <tokyonote9@gmail.com>
2020-02-21 17:36:41 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -3,9 +3,9 @@ require 'pry'
 def my_find(collection)
   i = 0
   while i < collection.length
-  lection[i]
+  
     if yield(collection[i])
       return col
-    i = i + 1
+    i = i + 1lection[i]
   end
 end
